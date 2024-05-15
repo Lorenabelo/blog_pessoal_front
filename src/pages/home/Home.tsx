a4e0672324655d4032bda6b3dabdebefd1dc7752
Lorena Belo <loore.belo@gmail.com>
2024-05-15 17:52:34 -0300

    feat: added initial structure and home component

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './Home.css';
+export default function Home () {
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://i.imgur.com/H88yIo2.png" alt="Imagem Tela Inicial" className="img" />
+        </>
+    )
+}
