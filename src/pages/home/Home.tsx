f03fe6b8973efcd311366502ebe80d52e1656fca
Lorena Belo <loore.belo@gmail.com>
2024-05-19 21:46:06 -0300

    feat: added styling and routing

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,28 @@
-import React from "react";
-import './Home.css';
-export default function Home () {
-    return (
-        <>
-            <h1 className="titulo">Home</h1>
-            <img src="https://i.imgur.com/H88yIo2.png" alt="Imagem Tela Inicial" className="img" />
-        </>
-    )
+import "./Home.css";
+export default function Home() {
+  return (
+    <>
+      <div className="bg-indigo-900 flex justify-center">
+        <div className="container grid grid-cols-2 text-white">
+          <div className="flex flex-col gap-4 items-center justify-center py-4">
+            <div className="max-w-7xl flex flex-col items-center">
+                <h2 className="text-5xl font-bold">Seja bem vindo(a)!</h2>
+                <p className="text-xl">Expresse aqui seus pensamentos e opniões</p>
+            </div>
+
+            <div className="flex justify-around gap-4">
+                <button className="rounded bg-white text-blue-800 py-2 px-4">
+                    Ver postagens
+                </button>
+            </div>
+          </div>
+
+          <div className="flex justify-center ">
+            <img src="https://i.imgur.com/VpwApCU.png" alt="Image home page" className="w-2/3" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
+
